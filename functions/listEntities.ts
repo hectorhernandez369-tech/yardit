b7b4896614c6cdaa87bd1987aff7eb94e10667e6
Hector Hernandez <hectorhernandez369@gmail.com>
2026-02-27 00:01:07 +0000

    File changes

--- functions/listEntities.ts
+++ functions/listEntities.ts
@@ -1,7 +1,11 @@
-export default async function fetch(req) {
-  const dir = [];
-  for await (const dirEntry of Deno.readDir('./entities')) {
-    dir.push(dirEntry.name);
+Deno.serve(async (req) => {
+  try {
+    const dir = [];
+    for await (const dirEntry of Deno.readDir('./entities')) {
+      dir.push(dirEntry.name);
+    }
+    return Response.json(dir);
+  } catch (err) {
+    return Response.json({ error: err.message });
   }
-  return Response.json(dir);
-}
+});
