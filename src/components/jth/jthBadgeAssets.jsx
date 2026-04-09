a614a421b8df53e34df9e1502883634c6009c57a
Hector Hernandez <hectorhernandez369@gmail.com>
2026-04-09 01:24:01 +0000

    File changes

--- src/components/jth/jthBadgeAssets.jsx
+++ src/components/jth/jthBadgeAssets.jsx
@@ -1,14 +1,14 @@
 export const JTH_BADGE_ASSETS = {
-  scout: "https://media.base44.com/images/public/690f554506edf795e5d84121/af4a60247_file_0000000096ac71f58cd6621423cc0f81.png#xywh=0,0,204,176",
-  tracker: "https://media.base44.com/images/public/690f554506edf795e5d84121/af4a60247_file_0000000096ac71f58cd6621423cc0f81.png#xywh=204,0,204,176",
-  seeker: "https://media.base44.com/images/public/690f554506edf795e5d84121/af4a60247_file_0000000096ac71f58cd6621423cc0f81.png#xywh=408,0,204,176",
-  pathfinder: "https://media.base44.com/images/public/690f554506edf795e5d84121/af4a60247_file_0000000096ac71f58cd6621423cc0f81.png#xywh=612,0,204,176",
-  hunter: "https://media.base44.com/images/public/690f554506edf795e5d84121/af4a60247_file_0000000096ac71f58cd6621423cc0f81.png#xywh=816,0,204,176",
-  gold_hunter: "https://media.base44.com/images/public/690f554506edf795e5d84121/af4a60247_file_0000000096ac71f58cd6621423cc0f81.png#xywh=0,176,204,176",
-  elite_hunter: "https://media.base44.com/images/public/690f554506edf795e5d84121/af4a60247_file_0000000096ac71f58cd6621423cc0f81.png#xywh=204,176,204,176",
-  trailblazer: "https://media.base44.com/images/public/690f554506edf795e5d84121/af4a60247_file_0000000096ac71f58cd6621423cc0f81.png#xywh=408,176,204,176",
-  master_of_the_hunt: "https://media.base44.com/images/public/690f554506edf795e5d84121/af4a60247_file_0000000096ac71f58cd6621423cc0f81.png#xywh=612,176,204,176",
-  legend_of_the_hunt: "https://media.base44.com/images/public/690f554506edf795e5d84121/af4a60247_file_0000000096ac71f58cd6621423cc0f81.png#xywh=816,176,204,176",
+  scout: "https://media.base44.com/images/public/690f554506edf795e5d84121/fa1458e38_file_0000000096ac71f58cd6621423cc0f811.png",
+  tracker: "https://media.base44.com/images/public/690f554506edf795e5d84121/420c67860_file_0000000096ac71f58cd6621423cc0f812.png",
+  seeker: "https://media.base44.com/images/public/690f554506edf795e5d84121/c0fec5a98_file_0000000096ac71f58cd6621423cc0f813.png",
+  pathfinder: "https://media.base44.com/images/public/690f554506edf795e5d84121/5d3e8af72_file_0000000096ac71f58cd6621423cc0f814.png",
+  hunter: "https://media.base44.com/images/public/690f554506edf795e5d84121/00bdf16f5_file_0000000043f871fd9b3b2469a3c295b91.png",
+  gold_hunter: "https://media.base44.com/images/public/690f554506edf795e5d84121/438c20839_file_0000000096ac71f58cd6621423cc0f815.png",
+  elite_hunter: "https://media.base44.com/images/public/690f554506edf795e5d84121/a07031cc8_file_0000000096ac71f58cd6621423cc0f816.png",
+  trailblazer: "https://media.base44.com/images/public/690f554506edf795e5d84121/06a3cbc8b_file_0000000096ac71f58cd6621423cc0f817.png",
+  master_of_the_hunt: "https://media.base44.com/images/public/690f554506edf795e5d84121/0bfe33d39_file_0000000096ac71f58cd6621423cc0f818.png",
+  legend_of_the_hunt: "https://media.base44.com/images/public/690f554506edf795e5d84121/b4946b40d_file_0000000043f871fd9b3b2469a3c295b92.png",
 };
 
 export function getJthBadgeAssetByRank(rankName = "") {
