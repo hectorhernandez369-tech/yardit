d3f47484472c464b3403ed22ce8e61b5a1ba8aaf
Hector Hernandez <hectorhernandez369@gmail.com>
2026-06-11 14:19:10 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -85,6 +85,7 @@ const AuthenticatedApp = () => {
             />
           )}
           <Route path="/ComingSoon" element={<ComingSoon />} />
+          <Route path="/events" element={<Events />} />
           <Route path="*" element={<Navigate to="/ComingSoon" replace />} />
         </Routes>
       </>
